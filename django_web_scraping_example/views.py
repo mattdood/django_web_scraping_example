aedbd41e57d522e7e0aaa5d8c5984a84eee748a7
mattdood <mawimberly@csumb.edu>
2020-07-19 17:57:30 -0700

    finished app

--- django_web_scraping_example/views.py
+++ django_web_scraping_example/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 
+from scraping.models import News # bring News into the views
+
 # Create your views here.
 
 
-class HomePageView(generic.TemplateView):
+class HomePageView(generic.ListView):
     template_name = 'home.html'
+    context_object_name = 'articles' # assign "News" object list to the object "articles"
+
+    # pass news objects as queryset for listview
+    def get_queryset(self):
+        return News.objects.all()
